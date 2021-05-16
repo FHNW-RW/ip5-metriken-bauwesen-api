71249bfa11efd4be9f8e1102198f03504cf6e991
Ralf Winkelmann <ralf.winkelmann@hotmail.com>
2021-05-16 17:30:05 +0200

    - added deserialization for model - first prediction without transforming data

--- app/routers/area_main_usage.py
+++ app/routers/area_main_usage.py
@@ -1,8 +1,37 @@
+import time
+
+import pandas as pd
+
 from fastapi import APIRouter
+from fastapi.encoders import jsonable_encoder
+from pydantic.main import BaseModel
+from joblib import load
+
 
 router = APIRouter(prefix="/hauptnutzflaeche")
+model = load('models/linear_reg_model.joblib')
 
 
-@router.get("/predict")
-async def predict():
-    return {"message": "Hello World"}
+class HNFPredictionInputs(BaseModel):
+    area_total_floor_416: int
+    area_main_usage: int
+
+
+@router.post("/predict/")
+async def predict(inputs: HNFPredictionInputs):
+    start = time.time()
+
+    # convert json to pandas dataframe
+    inputs_dict = jsonable_encoder(inputs)
+    for key, value in inputs_dict.items():
+        inputs_dict[key] = [value]
+
+    input_df = pd.DataFrame.from_dict(inputs_dict)
+    prediction = model.predict(input_df)[0]
+
+    exec_time = round((time.time() - start), 3)
+
+    return {
+        "prediction": prediction,
+        "exec_time": exec_time,
+    }
